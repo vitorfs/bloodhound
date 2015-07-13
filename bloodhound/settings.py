4ab9757415d23833d372537eb6c4d16dc9475a58
Vitor Freitas <vitorfs@gmail.com>
2015-07-13 19:12:13 +0300

    Update settings

--- bloodhound/settings.py
+++ bloodhound/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'bloodhound.me']
 
 
 # Application definition
